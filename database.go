7fbd968789f5a0f1cebc9b59e2dadf5df1172525
Roope Hongisto <roope.hongisto@markus.amberla>
2025-01-22 14:56:09 +0200

    added user table to database

--- database.go
+++ database.go
@@ -12,6 +12,14 @@ func initDB() *sql.DB {
 	db, err := sql.Open("sqlite3", "./forum.db")
 	if err != nil {
 		log.Fatal(err)
+		_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL,
+			email TEXT UNIQUE NOT NULL,
+			password TEXT NOT NULL
+		);
+	`)
 	}
 	return db
 }
