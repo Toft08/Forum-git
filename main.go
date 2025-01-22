635add1cb34156871b91c92bdf7b90c542f18a6f
Roope <roope.hongisto@gritlab.ax>
2025-01-22 15:44:53 +0200

    sign up should work now

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -60,8 +61,9 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Attempt to insert new user into database
-		_, err = db.Exec("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", username, email, hashedPassword)
+		_, err = db.Exec("INSERT INTO User (username, email, password, created_at) VALUES (?, ?, ?, ?)", username, email, hashedPassword, time.Now().Format("2006-01-02 15:04:05"))
 		if err != nil {
+			log.Println("Error inserting user:", err)
 			http.Error(w, "Email already exists", http.StatusBadRequest)
 			return
 		}
