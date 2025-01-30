c4d3c9ec2743229355547260b0dd8bf8be7f7356
fannielf <fanni.vesanen@gritlab.ax>
2025-01-30 15:07:11 +0200

    postpage handler

--- web/postpage.go
+++ web/postpage.go
@@ -17,32 +17,26 @@ func PostHandler(w http.ResponseWriter, r *http.Request, data *PageDetails) {
 		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
 	}
 
-	// Check the cookie and get userID
-	var userID int
-	data.LoggedIn, userID = VerifySession(r)
-	data.Posts = nil
-
-	if r.Method == http.MethodPost {
-		data.LoggedIn, _ = VerifySession(r)
-
-		if !data.LoggedIn {
-			ErrorHandler(w, "Unauthorized: You must be logged in to create a post", http.StatusUnauthorized)
-			return
-		}
-
-		content := r.FormValue("comment")
-
-		// Insert post into the database
-		_, err := db.Exec("INSERT INTO Comment (post_id, content, user_id, created_at) VALUES (?, ?, ?, ?)",
-			postID, content, userID, time.Now().Format("2006-01-02 15:04:05"))
-		if err != nil {
-			log.Println("Error creating post:", err)
-			ErrorHandler(w, "errorInCreatePost", http.StatusNotFound)
-			return
-		}
-	} else if r.Method != http.MethodGet {
-		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	valid := ValidatePostID(postID)
+	if !valid {
+		log.Println("Invalid postID")
+		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
 	}
+
+	switch r.Method {
+	case http.MethodGet:
+		HandlePostPageGet(w, r, data, postID)
+	case http.MethodPost:
+		HandlePostPagePost(w, r, data, postID)
+	default:
+		ErrorHandler(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func HandlePostPageGet(w http.ResponseWriter, r *http.Request, data *PageDetails, postID int) {
+	data.LoggedIn, _ = VerifySession(r)
+	data.Posts = nil
 
 	post, err := GetPostDetails(postID)
 	if err != nil {
@@ -54,5 +48,38 @@ func PostHandler(w http.ResponseWriter, r *http.Request, data *PageDetails) {
 	data.Posts = append(data.Posts, *post)
 
 	RenderTemplate(w, "post", data)
+}
 
+func HandlePostPagePost(w http.ResponseWriter, r *http.Request, data *PageDetails, postID int) {
+	var userID int
+	data.LoggedIn, userID = VerifySession(r)
+
+	if !data.LoggedIn {
+		ErrorHandler(w, "Unauthorized: You must be logged in to create a post", http.StatusUnauthorized)
+		return
+	}
+
+	// votes as Roope had them
+
+	content := r.FormValue("comment")
+
+	// Insert comment into the database
+	_, err := db.Exec("INSERT INTO Comment (post_id, content, user_id, created_at) VALUES (?, ?, ?, ?)",
+		postID, content, userID, time.Now().Format("2006-01-02 15:04:05"))
+	if err != nil {
+		log.Println("Error creating post:", err)
+		ErrorHandler(w, "errorInCreatePost", http.StatusNotFound)
+		return
+	}
+	HandlePostPageGet(w, r, data, postID)
+}
+
+func ValidatePostID(postID int) bool {
+	var post int
+	err := db.QueryRow("SELECT id FROM Post WHERE id = ?", postID).Scan(&post)
+	if err != nil {
+		log.Println("Error scanning postID:", err)
+		return false
+	}
+	return true
 }
