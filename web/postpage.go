61c996b32eee30b5519e410b3d131dc443ac3f29
fannielf <fanni.vesanen@gritlab.ax>
2025-01-31 12:31:33 +0200

    add votes to database

--- web/postpage.go
+++ web/postpage.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -34,6 +36,7 @@ func PostHandler(w http.ResponseWriter, r *http.Request, data *PageDetails) {
 
 }
 
+// HandlePostPageGet handles get requests to the post page
 func HandlePostPageGet(w http.ResponseWriter, r *http.Request, data *PageDetails, postID int) {
 	var userID int
 	data.LoggedIn, userID = VerifySession(r)
@@ -51,6 +54,7 @@ func HandlePostPageGet(w http.ResponseWriter, r *http.Request, data *PageDetails
 	RenderTemplate(w, "post", data)
 }
 
+// HandlePostPagePost handles post requests to the post page
 func HandlePostPagePost(w http.ResponseWriter, r *http.Request, data *PageDetails, postID int) {
 	var userID int
 	data.LoggedIn, userID = VerifySession(r)
@@ -73,6 +77,7 @@ func HandlePostPagePost(w http.ResponseWriter, r *http.Request, data *PageDetail
 	HandlePostPageGet(w, r, data, postID)
 }
 
+// ValidatePostID checks if a post with the given ID exists in the database
 func ValidatePostID(postID int) bool {
 	var post int
 	err := db.QueryRow("SELECT id FROM Post WHERE id = ?", postID).Scan(&post)
@@ -81,4 +86,50 @@ func ValidatePostID(postID int) bool {
 		return false
 	}
 	return true
+}
+
+// AddVotes adds or updates a vote type for a post or comment
+func AddVotes(userID, postID, commentID, vote int) error {
+
+	if postID == 0 && commentID == 0 {
+		return fmt.Errorf("both postID and commentID cannot be zero")
+	}
+
+	query := `SELECT Type FROM Like WHERE user_id = ? AND (post_id = ? OR comment_id = ?)`
+	row := db.QueryRow(query, userID, postID, commentID)
+	var likeType int
+	err := row.Scan(&likeType)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			likeType = -1 // To imply that no record exists
+		} else {
+			log.Println("Error scanning current value:", err)
+			return err
+		}
+	}
+	if likeType == vote {
+		// If existing like type is the same the the current, remove the like by changing the type to 0
+		updateQuery := `UPDATE Like SET type = 0 WHERE user_id = ? AND (post_id = ? OR comment_id = ?)`
+		_, err = db.Exec(updateQuery, userID, postID, commentID)
+		if err != nil {
+			log.Println("Error updating the record to 0:", err)
+			return err
+		}
+	} else if likeType == -1 {
+		// If no record exists, insert a new one
+		insertQuery := `INSERT INTO Like (type, user_id, post_id, comment_id) VALUES (?, ?, ?, ?)`
+		_, err = db.Exec(insertQuery, vote, userID, postID, commentID)
+		if err != nil {
+			log.Println("Error inserting record:", err)
+			return err
+		}
+	} else {
+		updateQuery := `UPDATE Like SET type = ? WHERE user_id = ? AND (post_id = ? OR comment_id = ?)`
+		_, err = db.Exec(updateQuery, vote, userID, postID, commentID)
+		if err != nil {
+			log.Println("Error updating the record to new vote:", err)
+			return err
+		}
+	}
+	return nil
 }
