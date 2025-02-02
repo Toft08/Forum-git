60331d16901d1b4389d740e75f219af201abf21c
Roope Hongisto <roope@Roopes-MacBook-Air.local>
2025-02-02 13:46:52 +0200

    cherrys

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"forum/database"
 	"forum/web"
 	"log"
 	"net/http"
@@ -10,8 +11,17 @@ func main() {
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
+	db := database.InitDB()
+	defer db.Close()
+
+	web.MakeTables(db)
+
 	http.HandleFunc("/", web.PageHandler)
 
 	log.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error starting the server:", err)
+	}
 }
